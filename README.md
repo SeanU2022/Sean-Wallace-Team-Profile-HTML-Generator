132df68fa4af18e0a2162fdeec088b3e150b7d02
Sean Wallace <sean.wallace.australia@gmail.com>
2022-10-09 01:50:08 +1100

    initialise project

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Sean-Wallace-Team-Profile-HTML-Generator
-NodeJS CLI App using Inquirer to capture information that generates an HTML webpage
+# Sean Wallace Team Profile HTML Generator
+-----
+![image](https://img.shields.io/badge/License-MIT-yellow.svg)
+## Description
+A Node.js CLI Application using Inquirer to capture information about staff in a software engineering team that generates an HTML webpage displaying cards with summaries for each person.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licence](#licence)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+-----
+
+## Installation
+package.json dependencies: "inquirer": "^8.2.4" & "jest": "^24.9.0"
+package.json scripts: "test": "jest"
+run npm install in project home folder
+  
+## Usage
+  
+run node index and follow prompts
+
+
+## Licence
+
+MIT
+
+![image](https://img.shields.io/badge/License-MIT-yellow.svg) An explanation of the licence is here: https://opensource.org/licenses/MIT
+
+
+
+## Contributing
+  
+contact creator for information
+
+-----
+
+## Tests
+
+from project folder/"npm run test" to run the test suite
+
+-----
+
+## RepoInfo
+Move the generated HTML file before you overwrite it
+
+## Questions
+
+You can find out more at my GitHub account: <https://github.com/SeanU2022>
+
+Please send any more questions you may have to: <sean.wallace.australia@gmail.com>
---
+++ index.js
@@ -0,0 +1,88 @@
+(function () {
+    console.log('hello world')
+    console.log(this)
+})
+()
+
+fnName = () => {
+    console.log('arrow function')
+    console.log('arrow function')
+}
+fnName()  // this call the anonynous fn
+
+fnDoStuff = (param1, param2) => {
+    console.log('arrow function param1 ' + param1)
+    console.log('arrow function param2 ' + param2)
+}
+fnDoStuff('P1', 222)
+
+
+const inquirer = require('inquirer');
+// Node v10+ includes a promises module as an alternative to using callbacks with file system methods.
+const { writeFile } = require('fs').promises;
+
+// MOVE LATER
+//const { generateMarkdown, licences } = require('./utils/generateHTML')
+
+// Array of questions for user input
+// WARNING: DO NOT USE kebab-case for name
+// {
+//     type: 'list', 
+//     name: 'licence',
+//     message: 'What kind of licence should your project have?',
+//     // lic is used as an example to a new developer
+//     choices: licences.map(lic => lic.name),
+//     default: "MIT"
+// },
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input', 
+            name: 'repoinfo',
+            message: 'What does the user need to know about using the repo?'
+        }
+])
+}
+
+// const generateHTML = ({ name, location, github, linkedin }) =>
+const generateHTML = ({ name, repoinfo }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Team Profile</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${name}</h1>
+    <p class="lead">I am from ${repoinfo}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${name}</li>
+      <li class="list-group-item">LinkedIn: ${repoinfo}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+// Bonus using writeFileSync as a promise
+const init = () => {
+    promptUser()
+      // Use writeFile method imported from fs.promises to use promises instead of
+      // a callback function
+      .then((answers) => writeFile('index.html', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err))
+}
+  
+init()
